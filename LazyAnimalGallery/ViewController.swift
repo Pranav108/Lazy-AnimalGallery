f2d46efd8d7b8aece49a357a06cf7bdd6aa74cdb
Pranav Pratap Singh <pranavpratap@paisabazaar.com>
2023-10-31 10:26:52 +0530

    Initial Commit

---
+++ LazyAnimalGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LazyAnimalGallery
+//
+//  Created by Pranav Pratap on 31/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
